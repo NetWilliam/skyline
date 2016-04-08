c6a6877e2398d039eb0899b98f3a72e16d79395e
NetWilliam <NWm.LWB@gmail.com>
2016-04-08 17:05:50 +0800

    [modify] quit whole process while a thread or greenlet quit

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='skymonitor',
 
-    version='0.0.8',
+    version='0.0.10',
 
     description='skyline monitor',
     long_description=long_description,

130ef5c063e8f81e877808890c5d9a73901a6f88
NetWilliam <NWm.LWB@gmail.com>
2016-04-12 14:27:19 +0800

    [modify] update pip package version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='skymonitor',
 
-    version='0.0.13',
+    version='0.0.14',
 
     description='skyline monitor',
     long_description=long_description,

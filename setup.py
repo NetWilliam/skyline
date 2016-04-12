6eef4da9dee1fdb51aeeed7cd736dc5c4ae5681a
NetWilliam <NWm.LWB@gmail.com>
2016-04-12 15:14:10 +0800

    [add] add verify function

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='skymonitor',
 
-    version='0.0.14',
+    version='0.0.15',
 
     description='skyline monitor',
     long_description=long_description,
@@ -31,7 +31,8 @@ setup(
                             "skygather = skyline_client:gather_main",
                             "skymonitor_worker = skyline_client:monitor_worker_main",
                             "skysummary_worker = skyline_client:summary_worker_main",
-                            "skywarning_worker = skyline_client:warning_worker_main"]
+                            "skywarning_worker = skyline_client:warning_worker_main",
+                            "skyverify = skyline_client:verify_main"]
     },
 
     license='MIT',
